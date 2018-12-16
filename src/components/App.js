860d56d18877a3fa829c751572113353565281b1
Nicole <nicole.sattler@live.com>
2018-12-16 12:10:41 -0600

    Remove import

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React, { Component, Fragment } from 'react'
 import Header from './Header'
-import AppInfo from './AppInfo'
 import AppInfoWrapper from './AppInfoWrapper'
 
 class App extends Component {
