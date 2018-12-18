6e241734a5b03b718ec37d555f4a0576c116bfa5
Nicole <nicole.sattler@live.com>
2018-12-18 17:09:46 -0600

    Change directory

---
+++ src/components/AppInfoWrapper/AppInfoWrapper.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import AppInfo from '../AppInfo'
+
+class AppInfoWrapper extends Component {
+  render() {
+    const extensionList = this.props.extensions.map((ext, i) => (
+      <AppInfo key={i} index={i} ext={ext} />
+    ))
+    return (
+      <ul>
+        <div className="section-title">
+          <h3>Extensions</h3>
+          <span className="turn-off" onClick={() => this.props.disableAll()}>
+            Disable all
+          </span>
+        </div>
+
+        {extensionList}
+      </ul>
+    )
+  }
+}
+
+export default AppInfoWrapper
