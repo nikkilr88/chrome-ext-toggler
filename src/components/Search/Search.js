c303e6e73f5d303bbaf0962ff55864f2d9fbbdb6
Nicole Sattler <nicole.sattler@live.com>
2019-08-13 21:04:51 -0500

    Start hooks refactor

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,104 +1,169 @@
-import React, { Component } from 'react'
+import React, { Component, useState, useRef, useEffect } from 'react'
 import AppInfo from '../AppInfo'
 
-class Search extends Component {
-  state = {
-    inputValue: '',
-    currentIndex: -1
-  }
+const Search = ({ handleSearch, extensions, searchValue }) => {
+  const [inputValue, setInputValue] = useState('')
+  const [currentIndex, setCurrentIndex] = useState(-1)
 
-  handleChange = e => {
+  const inputRef = useRef(null)
+
+  const handleChange = e => {
     e.persist()
 
     if (/\\/.test(e.target.value)) return
 
-    this.props.handleSearch(e)
-
-    this.setState(() => ({
-      inputValue: e.target.value,
-      currentIndex: -1
-    }))
+    handleSearch(e)
+    setCurrentIndex(-1)
+    setInputValue(e.target.value)
   }
 
-  switchFocus = e => {
-    const li = this.ul ? this.ul.childNodes : []
-
-    const arrowKeyPress = e.which === 40 || e.which === 38
-    const arrowDownPress = e.which === 38 && this.state.currentIndex >= 0
-    const arrowUpPress =
-      e.which === 40 && this.state.currentIndex < li.length - 1
-
-    this.setState(prevState => ({
-      currentIndex: arrowDownPress
-        ? prevState.currentIndex + -1
-        : arrowUpPress
-        ? prevState.currentIndex + 1
-        : arrowKeyPress
-        ? -1
-        : prevState.currentIndex
-    }))
-
-    if (arrowKeyPress && this.state.inputValue.length > 0) {
-      if (this.state.currentIndex === -1) {
-        this.searchInput.focus()
-      } else {
-        li[this.state.currentIndex].focus()
-      }
-    }
-  }
-
-  preventScroll = e => {
+  const preventScroll = e => {
     if (e.which === 38 || e.which === 40) {
       e.preventDefault()
       return false
     }
   }
 
-  componentDidMount() {
-    this.searchInput.focus()
+  useEffect(() => {
+    inputRef.current.focus()
 
-    document.addEventListener('keydown', this.preventScroll, false)
-    document.addEventListener('keyup', this.switchFocus, false)
-  }
+    document.addEventListener('keydown', preventScroll, false)
+    // document.addEventListener('keyup', this.switchFocus, false)
 
-  componentWillUnmount() {
-    document.removeEventListener('keydown', this.preventScroll, false)
-    document.removeEventListener('keyup', this.switchFocus, false)
-  }
+    return () => {
+      document.removeEventListener('keydown', preventScroll, false)
+      // document.removeEventListener('keyup', this.switchFocus, false)
+    }
+  }, [])
 
-  render() {
-    const { handleSearch, extensions, searchValue } = this.props
-    const searchReg = new RegExp(searchValue, 'gi')
+  const searchReg = new RegExp(searchValue, 'gi')
 
-    const searchResults = extensions
-      .filter(ext => {
-        if (ext.name.match(searchReg)) {
-          return ext
-        }
-      })
-      .sort((a, b) => a.name.localeCompare(b.name))
-      .map((ext, i) => <AppInfo key={i} index={i} ext={ext} tabIndex="1" />)
+  const searchResults = extensions
+    .filter(ext => {
+      if (ext.name.match(searchReg)) {
+        return ext
+      }
+    })
+    .sort((a, b) => a.name.localeCompare(b.name))
+    .map((ext, i) => <AppInfo key={i} index={i} ext={ext} tabIndex="1" />)
 
-    return (
-      <div className="search-wrapper">
-        <input
-          type="text"
-          onChange={e => this.handleChange(e)}
-          className="search"
-          spellCheck={false}
-          value={this.state.inputValue}
-          ref={input => {
-            this.searchInput = input
-          }}
-        />
-        {searchValue.length > 0 && (
-          <ul className="search-results" ref={ul => (this.ul = ul)}>
-            {searchResults.length > 0 ? searchResults : <p>No Results :(</p>}
-          </ul>
-        )}
-      </div>
-    )
-  }
+  return (
+    <div className="search-wrapper">
+      <input
+        type="text"
+        ref={inputRef}
+        className="search"
+        spellCheck={false}
+        value={inputValue}
+        onChange={e => handleChange(e)}
+      />
+      {searchValue.length > 0 && (
+        <ul className="search-results">
+          {searchResults.length > 0 ? searchResults : <p>No Results :(</p>}
+        </ul>
+      )}
+    </div>
+  )
 }
+
+// class Search extends Component {
+//   state = {
+//     inputValue: "",
+//     currentIndex: -1
+//   };
+
+//   handleChange = e => {
+//     e.persist();
+
+//     if (/\\/.test(e.target.value)) return;
+
+//     this.props.handleSearch(e);
+
+//     this.setState(() => ({
+//       inputValue: e.target.value,
+//       currentIndex: -1
+//     }));
+//   };
+
+//   switchFocus = e => {
+//     const li = this.ul ? this.ul.childNodes : [];
+
+//     const arrowKeyPress = e.which === 40 || e.which === 38;
+//     const arrowDownPress = e.which === 38 && this.state.currentIndex >= 0;
+//     const arrowUpPress =
+//       e.which === 40 && this.state.currentIndex < li.length - 1;
+
+//     this.setState(prevState => ({
+//       currentIndex: arrowDownPress
+//         ? prevState.currentIndex + -1
+//         : arrowUpPress
+//         ? prevState.currentIndex + 1
+//         : arrowKeyPress
+//         ? -1
+//         : prevState.currentIndex
+//     }));
+
+//     if (arrowKeyPress && this.state.inputValue.length > 0) {
+//       if (this.state.currentIndex === -1) {
+//         this.searchInput.focus();
+//       } else {
+//         li[this.state.currentIndex].focus();
+//       }
+//     }
+//   };
+
+//   preventScroll = e => {
+//     if (e.which === 38 || e.which === 40) {
+//       e.preventDefault();
+//       return false;
+//     }
+//   };
+
+//   componentDidMount() {
+//     this.searchInput.focus();
+
+//     document.addEventListener("keydown", this.preventScroll, false);
+//     document.addEventListener("keyup", this.switchFocus, false);
+//   }
+
+//   componentWillUnmount() {
+//     document.removeEventListener("keydown", this.preventScroll, false);
+//     document.removeEventListener("keyup", this.switchFocus, false);
+//   }
+
+//   render() {
+//     const { handleSearch, extensions, searchValue } = this.props;
+//     const searchReg = new RegExp(searchValue, "gi");
+
+//     const searchResults = extensions
+//       .filter(ext => {
+//         if (ext.name.match(searchReg)) {
+//           return ext;
+//         }
+//       })
+//       .sort((a, b) => a.name.localeCompare(b.name))
+//       .map((ext, i) => <AppInfo key={i} index={i} ext={ext} tabIndex="1" />);
+
+//     return (
+//       <div className="search-wrapper">
+//         <input
+//           type="text"
+//           onChange={e => this.handleChange(e)}
+//           className="search"
+//           spellCheck={false}
+//           value={this.state.inputValue}
+//           ref={input => {
+//             this.searchInput = input;
+//           }}
+//         />
+//         {searchValue.length > 0 && (
+//           <ul className="search-results" ref={ul => (this.ul = ul)}>
+//             {searchResults.length > 0 ? searchResults : <p>No Results :(</p>}
+//           </ul>
+//         )}
+//       </div>
+//     );
+//   }
+// }
 
 export default Search
